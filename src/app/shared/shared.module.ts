021fbad22220d0fa155ee0c77fefd05349bea951
Sqwezzy33333 <sergey.kornev3@gmail.com>
2023-04-27 17:23:28 +0300

    feat: add footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 
 import { HeaderComponent } from './components/header/header.component';
-
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}

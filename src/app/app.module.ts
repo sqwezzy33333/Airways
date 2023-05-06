d2f2cf4f9afc65ac5249ad2c1bf1ed8ed34bd1bd
JenkJS <jenkjobs@gmail.com>
2023-05-06 23:51:47 +0300

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {  BookingComponent, HomeComponent, ShopingCartComponent} from './pages/index';
+import {  BookingComponent, ShopingCartComponent} from './pages/index';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { HomeModule } from './pages/home/home.module';
+import {HomeModule} from "./pages/home/home.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     BookingComponent,
-    HomeComponent,
     ShopingCartComponent
   ],
   imports: [

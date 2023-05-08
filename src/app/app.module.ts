b6faad8e68361f623ced2a95f47df50a00d95564
Sqwezzy33333 <sergey.kornev3@gmail.com>
2023-05-08 14:31:16 +0300

    feat: create step2 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {  BookingComponent, ShopingCartComponent} from './pages/index';
+import { BookingComponent, ShopingCartComponent } from './pages/index';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import {HomeModule} from "./pages/home/home.module";
+import { HomeModule } from './pages/home/home.module';
+import { Step2Component } from './pages/booking/step2/step2/step2.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookingComponent,
-    ShopingCartComponent
+    ShopingCartComponent,
+    Step2Component,
   ],
   imports: [
     BrowserModule,
@@ -21,9 +24,9 @@ import {HomeModule} from "./pages/home/home.module";
     SharedModule,
     CoreModule,
     HomeModule,
+    MaterialModule
   ],
-  providers: [
-],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/booking/step2/step2/step2.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-step2',
+  templateUrl: './step2.component.html',
+  styleUrls: ['./step2.component.scss'],
+})
+export class Step2Component implements OnInit {
+  stepForm: FormGroup = new FormGroup({});
+
+  value = 'Clear me';
+
+  passArrayOfString: string[] = [];
+
+  step1Obj = {
+    tripType: 'roundTrip',
+    from: {
+      country: 'Andorra',
+      calling_code: 376,
+    },
+    dest: {
+      country: 'Algeria',
+      calling_code: 213,
+    },
+    date: {
+      singleDate: '',
+      startDate: '2023-05-15T21:00:00.000Z',
+      endDate: '2023-05-22T21:00:00.000Z',
+    },
+    passengers: {
+      adult: 2,
+      child: 1,
+      infant: 1,
+    },
+  };
+
+  passengers = Object.entries(this.step1Obj.passengers);
+
+  ngOnInit(): void {
+    this.transformPass();
+    this.createForm();
+    console.log(this.passArrayOfString);
+    console.log(this.arrayForms);
+  }
+
+  constructor() {}
+
+  arrayForms: any[] = [];
+
+  transformPass() {
+    this.passengers.forEach((el) => {
+      if (el[1] !== 0) {
+        while (el[1] > 0) {
+          this.passArrayOfString.push(el[0]);
+          el[1]--;
+        }
+      }
+    });
+  }
+
+  createForm() {
+    let allForms: any = {};
+    this.passArrayOfString.forEach((el, index) => {
+      let itemArray = [];
+      let item: string = el + (index + 1);
+      let spanText: string = index + 1 + '. ' + this.capitalizeFirstLetter(el);
+
+      allForms = new FormGroup({
+        [item + 'FirstName']: new FormControl<string>('', Validators.required),
+        [item + 'LastName']: new FormControl<string>('', Validators.required),
+        [item + 'Gender']: new FormControl<string>('', Validators.required),
+        [item + 'BirthdayDate']: new FormControl<string>(
+          '',
+          Validators.required
+        ),
+      });
+      let keys = Object.keys(allForms.controls);
+      itemArray.push(spanText, allForms, keys);
+      this.arrayForms.push(itemArray);
+    });
+  }
+
+  capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  submitForm(){
+    console.log(this.arrayForms)
+  }
+}

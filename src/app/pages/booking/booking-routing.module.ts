afbab11d7c4fc36544f88952f310ef45f89ec043
Mariya <mashka00@inbox.ru>
2023-05-10 15:21:42 +0300

    feat: add children in booking routing

--- src/app/pages/booking/booking-routing.module.ts
+++ src/app/pages/booking/booking-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookingComponent } from '../../pages/index';
+import { BookingComponent, BookingPassengersComponent } from '../../pages/index';
 
 const routes: Routes = [
   {
-    path: '', component: BookingComponent
+    path: '', component: BookingComponent,
+    children: [
+      // { path: 'flights', component: BookingJoyrneyComponent },
+      { path: 'passengers', component: BookingPassengersComponent },
+      // { path: 'review-payment', component: BookingReviewPaymentComponent },
+    ]
   }
 ];
 

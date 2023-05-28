0c391a33bb5970f8d9736c0f8935815aa778a8d7
Sqwezzy33333 <sergey.kornev3@gmail.com>
2023-05-28 14:19:34 +0300

    feat: add review page

--- src/app/pages/booking/components/booking-review/booking-review.component.ts
+++ src/app/pages/booking/components/booking-review/booking-review.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PassengersFormStateService } from 'src/app/core/services/booking-passengers-state/booking-passengers-state.service';
 import { FlightsStateService } from 'src/app/core';
+import { FlightsResponse } from 'src/app/core';
 
 @Component({
   selector: 'app-booking-review',
@@ -9,50 +10,81 @@ import { FlightsStateService } from 'src/app/core';
 })
 export class BookingReviewComponent implements OnInit {
   passengers: any[] = [];
+  thereFlight!: FlightsResponse;
+  backFlight!: FlightsResponse;
 
   constructor(
     private passService: PassengersFormStateService,
     private flightState: FlightsStateService
   ) {}
+
   ngOnInit(): void {
     this.createPassengersInfo();
-    console.log(this.passengers);
+
+    this.flightState.thereFlight.subscribe((el) => {
+      this.thereFlight = el;
+    });
+
+    this.flightState.backFlight.subscribe((el) => {
+      if (Object.entries(el).length !== 0) {
+        this.backFlight = el;
+        this.isRoundTrip = true;
+      }
+    });
+
+    this.createFares();
   }
 
-  isOneWayTrip = false;
+  isRoundTrip: boolean = false;
 
-  exampleFares = [
-    {
-      type: 'Adult',
-      fare: 166,
-      tax: 91.3,
-      total: 228,
-    },
-    {
-      type: 'Child',
-      fare: 106,
-      tax: 90,
-      total: 228,
-    },
-    {
-      type: 'Infant',
-      fare: 88,
-      tax: 10.3,
-      total: 228,
-    },
-  ];
+  fares: any = [];
 
-  totalPrice = 500;
+  totalPrice: number = 0;
 
   createPassengersInfo() {
     this.passService.getPassengersForm().passengers.forEach((el: any) => {
+      let type: string = Object.keys(el)[0].split('-')[0];
       let onePassengers = {
         name: Object.values(el).splice(0, 2).join(' '),
         bag: '1checked bag (total 23 kg) included',
         cabinBag: '1 cabin bag + 1 personal item (max. 8 kg) included',
         seat: '12G',
+        type: type,
       };
       this.passengers.push(onePassengers);
     });
+  }
+
+  getFlightTime(date: Date) {
+    let hour = date.toString().split('T')[1].split(':')[0];
+    let minute = date.toString().split('T')[1].split(':')[1];
+    return hour + ':' + minute;
+  }
+
+  createFares() {
+    if (this.isRoundTrip) {
+      this.passengers.forEach((el) => {
+        let onePerson = {
+          type: el.type,
+          firstPrice: this.thereFlight.price.eur,
+          backPrice: this.backFlight.price.eur,
+          totalFlightPrice:
+            this.thereFlight.price.eur + this.backFlight.price.eur,
+        };
+        this.fares.push(onePerson);
+        this.totalPrice = this.totalPrice + onePerson.totalFlightPrice;
+      });
+    } else {
+      this.passengers.forEach((el) => {
+        let onePerson = {
+          type: el.type,
+          firstPrice: this.thereFlight.price.eur,
+          backPrice: 0,
+          totalFlightPrice: this.thereFlight.price.eur + 0,
+        };
+        this.fares.push(onePerson);
+        this.totalPrice = this.totalPrice + onePerson.totalFlightPrice;
+      });
+    }
   }
 }
